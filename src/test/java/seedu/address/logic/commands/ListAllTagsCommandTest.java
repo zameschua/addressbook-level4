520b80659b4aa6ab7e3dde20dbda19e77397f168
Pj Poh <e0037036@u.nus.edu>
2017-10-09 18:07:36 +0800

    Add ListAllTagsTest that will test that execute(), throws an Exception.

---
+++ src/test/java/seedu/address/logic/commands/ListAllTagsCommandTest.java
@@ -0,0 +1,35 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.ListAllTagsCommand.MESSAGE_NOT_IMPLEMENTED_YET;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+* Contains integration tests (interaction with the Model) and unit tests for ListAllTagsCommand.
+*/
+public class ListAllTagsCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute() throws Exception {
+        assertCommandFailure(prepareCommand(), model, MESSAGE_NOT_IMPLEMENTED_YET);
+    }
+
+   /**
+    * Returns an {@code ListAllTagsCommand}.
+    */
+    private ListAllTagsCommand prepareCommand() {
+        ListAllTagsCommand listAllTagsCommand = new ListAllTagsCommand();
+        listAllTagsCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+        return listAllTagsCommand;
+   }
+}
