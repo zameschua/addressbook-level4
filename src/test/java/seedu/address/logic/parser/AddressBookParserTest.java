3a4cb9a8b9a6fd7616348ec33f43917c2b0dcbb1
Pj Poh <e0037036@u.nus.edu>
2017-10-09 18:19:04 +0800

    Add new test to AddressBookParserTest, which will test that typing "listalltags" returns an instance of ListAllTagsCommand.

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -25,6 +25,7 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListAllTagsCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.UndoCommand;
@@ -107,6 +108,12 @@ public class AddressBookParserTest {
     public void parseCommand_list() throws Exception {
         assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
         assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
+    }
+
+    @Test
+    public void parseCommand_listAllTags() throws Exception {
+        ListAllTagsCommand command = (ListAllTagsCommand) parser.parseCommand(ListAllTagsCommand.COMMAND_WORD);
+        assertTrue(command instanceof ListAllTagsCommand);
     }
 
     @Test
