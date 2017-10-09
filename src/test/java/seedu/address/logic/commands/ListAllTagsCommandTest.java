062a7d3470512a9642684b5e674e6749a7f960df
Pj Poh <e0037036@u.nus.edu>
2017-10-09 18:57:54 +0800

    Remove unnecessary import statement in ListAllTagsCommandTest.

--- src/test/java/seedu/address/logic/commands/ListAllTagsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListAllTagsCommandTest.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.ListAllTagsCommand.MESSAGE_SUCCESS;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Before;
