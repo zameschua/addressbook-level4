23f5a1819f33b4c8d15c3863d1aea36648e7416f
Pj Poh <e0037036@u.nus.edu>
2017-10-09 20:00:52 +0800

    Take in UniqueTagList in ListAllTagsCommand in preparation for listing all tags.

--- src/main/java/seedu/address/logic/commands/ListAllTagsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListAllTagsCommand.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands;
 
+import javafx.collections.ObservableList;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.tag.Tag;
 
 /**
  * List all tags that exist in software to user
@@ -13,6 +15,7 @@ public class ListAllTagsCommand extends Command {
 
     @Override
     public CommandResult execute() throws CommandException {
+        ObservableList<Tag> allTags = model.getAddressBook().getTagList();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
