d92157eedec9e4da80f58d0a9450f5ab0c71011b
Pj Poh <e0037036@u.nus.edu>
2017-10-16 21:31:56 +0800

    Removed unnecessary import statements

--- src/main/java/seedu/address/ui/TagCard.java
+++ src/main/java/seedu/address/ui/TagCard.java
@@ -1,9 +1,7 @@
 package seedu.address.ui;
 
-import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.tag.Tag;
