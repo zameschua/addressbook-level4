fd28d0b6166dde164899f8d766cd2c0a7fd6e654
Pj Poh <e0037036@u.nus.edu>
2017-10-16 22:39:14 +0800

    Modifying ListAllTagsCommand to show tags under input bar.

--- src/main/java/seedu/address/logic/commands/ListAllTagsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListAllTagsCommand.java
@@ -3,6 +3,8 @@ package seedu.address.logic.commands;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TAGS;
 
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
+import seedu.address.model.tag.UniqueTagList;
 
 /**
  * List all tags that exist in software to user
@@ -15,6 +17,8 @@ public class ListAllTagsCommand extends Command {
 
     @Override
     public CommandResult execute() throws CommandException {
+        StringBuilder sb = new StringBuilder();
+        allTags = AddressBook.getUniqueTagList();
         model.updateFilteredTagList(PREDICATE_SHOW_ALL_TAGS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
