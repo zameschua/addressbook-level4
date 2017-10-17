e3e3be8606db93998e8c95b58e29fd8379ed7ed9
Pj Poh <e0037036@u.nus.edu>
2017-10-17 18:25:27 +0800

    Trivial implementation of ListAllTags working successfully. All tags listed below Command Box.

--- src/main/java/seedu/address/logic/commands/ListAllTagsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListAllTagsCommand.java
@@ -2,9 +2,9 @@ package seedu.address.logic.commands;
 
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TAGS;
 
+import javafx.collections.ObservableList;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.AddressBook;
-import seedu.address.model.tag.UniqueTagList;
+import seedu.address.model.tag.Tag;
 
 /**
  * List all tags that exist in software to user
@@ -13,13 +13,18 @@ public class ListAllTagsCommand extends Command {
 
     public static final String COMMAND_WORD = "listalltags";
 
-    public static final String MESSAGE_SUCCESS = "Listed all tags";
+    public static final String MESSAGE_SUCCESS = "All tags listed! They are: ";
 
     @Override
     public CommandResult execute() throws CommandException {
-        StringBuilder sb = new StringBuilder();
-        allTags = AddressBook.getUniqueTagList();
         model.updateFilteredTagList(PREDICATE_SHOW_ALL_TAGS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        ObservableList<Tag> allTags = model.getFilteredTagList();
+
+        StringBuilder sb = new StringBuilder();
+        for (Tag tag : allTags) {
+            sb.append(tag.tagName + " ");
+        }
+
+        return new CommandResult(MESSAGE_SUCCESS + sb);
     }
 }
