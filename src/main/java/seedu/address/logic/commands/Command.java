4347d71a7d8319e232a7456f486af386aa7bd35e
Pj Poh <e0037036@u.nus.edu>
2017-10-17 18:56:03 +0800

    Remove redundant import statements.

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -5,11 +5,8 @@ import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.commands.ListAllTagsCommand;
 import seedu.address.model.Model;
 import seedu.address.model.tag.Tag;
-
-import java.util.List;
 
 /**
  * Represents a command with hidden internal logic and the ability to be executed.
