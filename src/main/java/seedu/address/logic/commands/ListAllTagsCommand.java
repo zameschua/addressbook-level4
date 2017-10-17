076cbd9a0f194df0d9abeca1c476040fc8d7e379
Pj Poh <e0037036@u.nus.edu>
2017-10-17 21:20:06 +0800

    Notify EventsCenter when listAllTags command is called.

--- src/main/java/seedu/address/logic/commands/ListAllTagsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListAllTagsCommand.java
@@ -3,6 +3,8 @@ package seedu.address.logic.commands;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TAGS;
 
 import javafx.collections.ObservableList;
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.events.ui.JumpToListAllTagsRequestEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.tag.Tag;
 
@@ -20,6 +22,7 @@ public class ListAllTagsCommand extends Command {
         model.updateFilteredTagList(PREDICATE_SHOW_ALL_TAGS);
         ObservableList<Tag> allTags = model.getFilteredTagList();
 
+        EventsCenter.getInstance().post(new JumpToListAllTagsRequestEvent());
         return new CommandResult(getMessageForTagListShownSummary(allTags));
     }
 }
