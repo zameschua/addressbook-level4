6f3f17c2c05106fc5cfc8da237d97a819d107a01
Pj Poh <e0037036@u.nus.edu>
2017-10-25 09:19:48 +0800

    Edit check style errors in PersonInfo.java.

--- src/main/java/seedu/address/ui/PersonInfo.java
+++ src/main/java/seedu/address/ui/PersonInfo.java
@@ -2,15 +2,10 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
-import org.fxmisc.easybind.EasyBind;
-
-import com.google.common.eventbus.Subscribe;
-
-import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.ReadOnlyPerson;
@@ -41,18 +36,18 @@ public class PersonInfo extends UiPart<Region> {
 
     @FXML
     private ImageView attendance;
-
-
-
+    
     public PersonInfo (ReadOnlyPerson person) {
         super(FXML);
         this.person = person;
+        profilePic.setImage(new Image(
+                "https://vignette.wikia.nocookie.net/disney/images/f/f6/MickeyArt.jpg/revision/latest/scale-to-width-down/250?cb=20130705054827"));
         name.setText(person.getName().toString());
         address.setText(person.getAddress().toString());
         phone.setText(person.getPhone().toString());
         email.setText(person.getEmail().toString());
-        profilePic.setImage(new Image("https://vignette.wikia.nocookie.net/disney/images/f/f6/MickeyArt.jpg/revision/latest/scale-to-width-down/250?cb=20130705054827"));
-        attendance.setImage(new Image("http://communityofhope.church/wp-content/uploads/2015/02/Next-Attendance-Graph.jpg"));
+        attendance.setImage(new Image(
+                "http://communityofhope.church/wp-content/uploads/2015/02/Next-Attendance-Graph.jpg"));
     }
 
 }
