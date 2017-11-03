5d00f77389fe6a148a187e33a32d9b50694026c8
Yilun <yilun95@gmail.com>
2017-11-03 10:37:50 +0800

    test fixes

--- src/test/java/seedu/address/ui/CalendarPanelTest.java
+++ src/test/java/seedu/address/ui/CalendarPanelTest.java
@@ -1,8 +1,9 @@
 package seedu.address.ui;
 
 import static guitests.guihandles.WebViewUtil.waitUntilCalendarLoaded;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static seedu.address.testutil.EventsUtil.postNow;
+import static seedu.address.ui.CalendarPanel.DEFAULT_CALENDAR_URL;
 
 import java.net.URL;
 
@@ -32,15 +33,18 @@ public class CalendarPanelTest extends GuiUnitTest {
 
     @Test
     public void display() throws Exception {
+        // default calendar page
+        URL expectedDefaultPageUrl = new URL(DEFAULT_CALENDAR_URL);
+        assertTrue(expectedDefaultPageUrl.equals(calendarPanelHandle.getLoadedUrl()));
 
         postNow(calendarRequestEvent);
 
-        //expected login page
+        //expected google login page
         URL expectedCalendarUrl = new URL("https://accounts.google.com/ServiceLogin?service=cl&"
                 + "passive=1209600&osid=1&continue=https://calendar.google.com/calendar/render&followup="
                 + "https://calendar.google.com/calendar/render&scc=1");
 
         waitUntilCalendarLoaded(calendarPanelHandle);
-        assertEquals(expectedCalendarUrl, calendarPanelHandle.getLoadedUrl());
+        assertTrue(expectedCalendarUrl.equals(calendarPanelHandle.getLoadedUrl()));
     }
 }
