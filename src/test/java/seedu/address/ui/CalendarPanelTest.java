05a649b930206c7ffb0c9298751220bdc8d34bd2
Yilun <yilun95@gmail.com>
2017-11-03 15:18:38 +0800

    fix tests

--- src/test/java/seedu/address/ui/CalendarPanelTest.java
+++ src/test/java/seedu/address/ui/CalendarPanelTest.java
@@ -33,7 +33,7 @@ public class CalendarPanelTest extends GuiUnitTest {
     public void display() throws Exception {
         // default calendar page
         URL expectedDefaultPageUrl = new URL(DEFAULT_CALENDAR_URL);
-        assertEquals(expectedDefaultPageUrl, calendarPanelHandle.getLoadedUrl());
+        assertEquals(expectedDefaultPageUrl.toString(), calendarPanelHandle.getLoadedUrl().toString());
 
     }
 }
