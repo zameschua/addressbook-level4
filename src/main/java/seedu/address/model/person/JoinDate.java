d850cabeb5a6a9e82d07fed809dc4dcaf3b17a3e
Unknown <reneeseetyinghui@gmail.com>
2017-11-06 01:40:31 +0800

    Join Date

---
+++ src/main/java/seedu/address/model/person/JoinDate.java
@@ -0,0 +1,24 @@
+package seedu.address.model.person;
+
+/**
+ * Represents when Person was added to addressbook.
+ * Guarantees: Every Person will have a Join Date.
+ */
+
+//@@author ReneeSeet
+
+public class JoinDate {
+
+    public String joinDate;
+
+    /**
+     * Default constructor 
+     * only used for testing
+     */
+    public JoinDate() {
+        joinDate = ;
+    }
+    
+    
+    
+}
