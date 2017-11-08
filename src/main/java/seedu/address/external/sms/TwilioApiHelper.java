e6b7ac74c335b7cd12cfe3fbe0434e438cfbef47
Zames <zames.chua@gmail.com>
2017-11-08 14:30:52 +0800

    Update UG and DG, and project portfolio for my parts

--- src/main/java/seedu/address/external/sms/TwilioApiHelper.java
+++ src/main/java/seedu/address/external/sms/TwilioApiHelper.java
@@ -1,26 +1,25 @@
 package seedu.address.external.sms;
 
 import com.twilio.Twilio;
-import com.twilio.exception.ApiException;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.ui.NewResultAvailableEvent;
-import seedu.address.external.sms.exceptions.SmsServiceException;
 
+//@@author zameschua
 /**
  * Helper class to handle the sending of SMS using Twilio API
  */
 public class TwilioApiHelper {
-    // I shouldn't be storing the Account SIDs here
+    // TODO I shouldn't be storing the Account SIDs here
     public static final String ACCOUNT_SID = "AC8e7d80947bd2e877013c66d99b0faa06";
     public static final String AUTH_TOKEN = "46abad64b64c0b29c468434ff69e36ca";
 
     private static final String MESSAGE_SMS_SUCCESS = "SMS Successfully sent";
     private static final String COUNTRY_CODE_SINGAPORE = "+65";
-    private static final String PHONE_NUMBER_LENGTH_SINGAPORE = 8;
-    private static final String PHONE_NUMBER_REGEX_SINGAPORE = "\+65\d{8}";
+    private static final String PHONE_NUMBER_REGEX_SINGAPORE = "\\+65\\d{8}";
+    private static final int PHONE_NUMBER_LENGTH_SINGAPORE = 8;
 
     /**
      * Initialises the Twilio API service
@@ -35,15 +34,11 @@ public class TwilioApiHelper {
      * @param message The message to send as the content of the SMS
      */
     public static void sendSms(String message, String smsReceipient) {
-        try {
-            smsReceipient = checkPhoneNumberFormat(smsReceipient);
-            assert smsReceipient.matches(PHONE_NUMBER_REGEX_SINGAPORE);
-            // TODO: Secure the from phone number properly
-            Message.creator(new PhoneNumber(smsReceipient), new PhoneNumber("+1 954-320-0045"), message).create();
-            EventsCenter.getInstance().post(new NewResultAvailableEvent(MESSAGE_SMS_SUCCESS));
-        } catch (ApiException e) {
-            throw new SmsServiceException(e.getMessage());
-        }
+        smsReceipient = checkPhoneNumberFormat(smsReceipient);
+        assert smsReceipient.matches(PHONE_NUMBER_REGEX_SINGAPORE);
+        // TODO: Secure the from phone number properly
+        Message.creator(new PhoneNumber(smsReceipient), new PhoneNumber("+1 954-320-0045"), message).create();
+        EventsCenter.getInstance().post(new NewResultAvailableEvent(MESSAGE_SMS_SUCCESS));
     }
 
     /**
