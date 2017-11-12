84bfdd7510f5d8d661d6a672a24610add5ae0df0
Pj Poh <e0037036@u.nus.edu>
2017-11-12 23:27:51 +0800

    Update ListAllTagsCommandTest

--- src/test/java/seedu/address/logic/commands/ListAllTagsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListAllTagsCommandTest.java
@@ -1,9 +1,21 @@
 package seedu.address.logic.commands;
 
+import org.junit.Rule;
+import seedu.address.commons.events.ui.JumpToListAllTagsRequestEvent;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.ui.testutil.EventsCollectorRule;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.deleteFirstPerson;
+import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import static seedu.address.logic.commands.ListAllTagsCommand.MESSAGE_SUCCESS;
+
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
@@ -22,6 +34,9 @@ public class ListAllTagsCommandTest {
     private Model expectedModel;
     private ListAllTagsCommand listAllTagsCommand;
 
+    @Rule
+    public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
+
     @Before
     public void setUp() {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
@@ -32,7 +47,19 @@ public class ListAllTagsCommandTest {
     }
 
     @Test
-    public void execute() throws Exception {
+    public void execute_personListUnfiltered_showsEverything() throws Exception {
         assertCommandSuccess(listAllTagsCommand, model, ListAllTagsCommand.MESSAGE_SUCCESS, expectedModel);
     }
+
+    @Test
+    public void execute_listAllTags_success() {
+        try {
+            CommandResult result = listAllTagsCommand.execute();
+        } catch (CommandException e) {
+            System.out.println("CommandException");
+        }
+        assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof JumpToListAllTagsRequestEvent);
+        assertTrue(eventsCollectorRule.eventsCollector.getSize() == 1);
+    }
+
 }
