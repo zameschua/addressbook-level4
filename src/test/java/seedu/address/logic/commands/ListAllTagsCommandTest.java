510d146a15b4071204d5258fdc8bdb98a2772662
Pj Poh <e0037036@u.nus.edu>
2017-11-13 09:50:37 +0800

    Rearrange import statements order.

--- src/test/java/seedu/address/logic/commands/ListAllTagsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListAllTagsCommandTest.java
@@ -1,14 +1,5 @@
 package seedu.address.logic.commands;
 
-import seedu.address.commons.events.ui.JumpToListAllTagsRequestEvent;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.CommandHistory;
-import seedu.address.logic.UndoRedoStack;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
-import seedu.address.ui.testutil.EventsCollectorRule;
-
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static org.junit.Assert.assertTrue;
@@ -16,6 +7,15 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import seedu.address.commons.events.ui.JumpToListAllTagsRequestEvent;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.ui.testutil.EventsCollectorRule;
 
 //@@author pohjie
 /**
@@ -27,9 +27,6 @@ public class ListAllTagsCommandTest {
     private Model expectedModel;
     private ListAllTagsCommand listAllTagsCommand;
 
-    @Rule
-    public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
-
     @Before
     public void setUp() {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
@@ -39,6 +36,9 @@ public class ListAllTagsCommandTest {
         listAllTagsCommand.setData(model, new CommandHistory(), new UndoRedoStack());
     }
 
+    @Rule
+    public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
+    
     @Test
     public void execute_listAllTags_success() {
         try {
