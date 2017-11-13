89ef85cd53988d592686250e163d1c682755f00f
Pj Poh <e0037036@u.nus.edu>
2017-11-13 14:15:37 +0800

    Create TagCardHandle.

---
+++ src/test/java/guitests/guihandles/TagCardHandle.java
@@ -0,0 +1,35 @@
+package guitests.guihandles;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+
+//@@author pohjie
+/**
+ * Provides a handle to a tag card in the tag list panel
+ */
+public class TagCardHandle extends NodeHandle<Node> {
+    private static final String ID_FIELD_ID = "#id";
+    private static final String TAGSTRING_FIELD_ID = "tagString";
+
+    private final Label idLabel;
+    private final Label tagStringLabel;
+
+    public TagCardHandle(Node cardNode) {
+        super(cardNode);
+
+        this.idLabel = getChildNode(ID_FIELD_ID);
+        this.tagStringLabel = getChildNode(TAGSTRING_FIELD_ID);
+    }
+
+    public String getId() {
+        return idLabel.getText();
+    }
+
+    public String getTagString() {
+        return tagStringLabel.getText();
+    }
+}
