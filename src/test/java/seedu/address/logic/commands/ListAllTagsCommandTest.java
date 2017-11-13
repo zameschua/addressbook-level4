096e9ef317094a410e115d60b6c4e913d9cbac9b
Pj Poh <e0037036@u.nus.edu>
2017-11-13 09:45:34 +0800

    Update import order statements for ListAllTagsCommandTest.

--- src/test/java/seedu/address/logic/commands/ListAllTagsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListAllTagsCommandTest.java
@@ -1,28 +1,21 @@
 package seedu.address.logic.commands;
 
-import org.junit.Rule;
 import seedu.address.commons.events.ui.JumpToListAllTagsRequestEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.ui.testutil.EventsCollectorRule;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.deleteFirstPerson;
-import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import static seedu.address.logic.commands.ListAllTagsCommand.MESSAGE_SUCCESS;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.ui.testutil.EventsCollectorRule;
+
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
 //@@author pohjie
 /**
